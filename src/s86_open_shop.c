a173d324d75c1bdb85cbcc08a910a670104dd915
Kevin Mills <kevin.mills226@gmail.com>
2017-07-14 21:40:01 -0400

    Minor renaming

--- src/s86_open_shop.c
+++ src/s86_open_shop.c
@@ -13,12 +13,13 @@ item_t* select_inventory (void) {
     u32 level = 0;
     
     for (u32 i = 0; i < 8; ++i)
-        if (checkflag(mart_advancement_level[i]))
+        if (checkflag(mart_advancement_flags[i]))
             ++level;
     
     return inventories[level];
 }
 
-u16 mart_advancement_level[] = {
+// flags for all the badges
+u16 mart_advancement_flags[] = {
     0x820, 0x821, 0x822, 0x823, 0x824, 0x825, 0x826, 0x827
 };
